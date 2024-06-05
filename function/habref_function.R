5621be06e58477f4590ad3d984aa2510d1678ac0
Augustin Soulard <augustinsoulard@yahoo.fr>
2024-06-05 12:13:50 +0200

    v0.1.1

--- function/habref_function.R
+++ function/habref_function.R
@@ -14,31 +14,21 @@ taxon_hab = function(FCD_TYPO = "28"){
   load_data("HABREF_70", "data/HABREF_70/HABREF_70.csv")
   load_data("HABREF_CORRESP_TAXON_70","data/HABREF_70/HABREF_CORRESP_TAXON_70.csv")
   
-  typo = HABREF_70 %>% filter(CD_TYPO == FCD_TYPO)
+  typo = HABREF_70 %>% filter(CD_TYPO %in% FCD_TYPO)
   
   
   typo_taxa = left_join(typo,HABREF_CORRESP_TAXON_70,by=c("CD_HAB"="CD_HAB_ENTRE"))
   return(typo_taxa)
 }
 ###############################################################################
-PVF2_TAXON = taxon_hab(28)
-
-
-
-#########################TEST FONCTION SEARCH
-
-taxa_search = "Pol ave"
-typo_taxa = PVF2_TAXON
-colomn_name = "NOM_CITE"
-colomn_name2 = "NOM_CITE_MATCH"
 
 
 ############################################################   find_taxon_hab    
 find_taxon_hab <- function(taxa_search,
-                           typo_taxa,colomn_name = "NOM_CITE",
+                           colomn_name = "NOM_CITE",
                            colomn_name2 = "NOM_CITE_MATCH",
-                           colhab = "LB_HAB_FR",
-                           FCD_TYPO = NULL){
+                           FCD_TYPO = 28){
+  typo_taxa = taxon_hab(FCD_TYPO = FCD_TYPO)
   # Utilisation de str_split avec un motif pour diviser le texte
   texte_split <- strsplit(taxa_search, " ")[[1]]  # Utilisation de [[1]] pour extraire le vecteur de chaînes
   
@@ -49,29 +39,71 @@ find_taxon_hab <- function(taxa_search,
   texte_pour_grep <- paste(texte_split, collapse = "")
   
   # Utilisation de grep pour trouver les correspondances dans la base de données
-  resultats_grep <- typo_taxa[grep(texte_pour_grep, typo_taxa[[colomn_name]],
-                                   paste0(typo_taxa[[colomn_name]]," ",typo_taxa[[colomn_name2]]), 
+  resultats_grep <- typo_taxa[grep(texte_pour_grep, typo_taxa$NOM_CITE,
+                                   paste0(typo_taxa$NOM_CITE," ",typo_taxa$NOM_CITE_MATCH), 
                                    ignore.case = TRUE)
                               , ]
-  resultats_hab = resultats_grep[colhab]
+  resultats_hab = resultats_grep %>% select(NOM_CITE,LB_CODE,LB_HAB_FR)
   
-  return(list(resultats_grep,resultats_hab))
+  return(resultats_hab)
 }
 ###############################################################################
 
-# Appel de la fonction avec l'exemple "Que ile"
-find_taxon_hab("Pol avic",PVF2_TAXON)
-
+########################################################## hab_join
+hab_join = function(liste_CD_HAB,
+                    FCD_TYPO=28){
+  
+  load_data("HABREF_CORRESP_HAB_70", "data/HABREF_70/HABREF_CORRESP_HAB_70.csv")
+  load_data("HABREF_70", "data/HABREF_70/HABREF_70.csv")
+  data_CD_HAB = data.frame(CD_HAB_ENTRE = liste_CD_HAB)
+  
+  # if(FCD_TYPO!=107){
+  #   CORRESP107 = HABREF_CORRESP_HAB_70 %>% 
+  #     filter(CD_TYPO_ENTRE == FCD_TYPO & CD_TYPO_SORTIE == 107) %>% 
+  #     select(CD_HAB_ENTRE,CD_HAB_107 = CD_HAB_SORTIE)
+  # }
+  
+  if(FCD_TYPO!=7){
+    CORRESP7 = HABREF_CORRESP_HAB_70 %>% 
+      filter(CD_TYPO_ENTRE == FCD_TYPO & CD_TYPO_SORTIE == 7) %>%
+      select(CD_HAB_ENTRE,CD_HAB_7 = CD_HAB_SORTIE)
+    data_CD_HAB=left_join(data_CD_HAB,CORRESP7,by="CD_HAB_ENTRE")
+    join7 = HABREF_70 %>% select(CD_HAB,CD_EUNIS12 = LB_CODE,LB_EUNIS12 = LB_HAB_FR)
+    data_CD_HAB = left_join(data_CD_HAB,join7,by=c("CD_HAB_7"="CD_HAB"))
+  }
+  
+  if(FCD_TYPO!=8){
+    CORRESP8 = HABREF_CORRESP_HAB_70 %>% 
+      filter(CD_TYPO_ENTRE == FCD_TYPO & CD_TYPO_SORTIE == 8) %>%
+      select(CD_HAB_ENTRE,CD_HAB_8 = CD_HAB_SORTIE)
+    data_CD_HAB=left_join(data_CD_HAB,CORRESP8,by="CD_HAB_ENTRE")
+    join8 = HABREF_70 %>% select(CD_HAB,CD_HIC = LB_CODE,LB_HIC = LB_HAB_FR)
+    data_CD_HAB = left_join(data_CD_HAB,join8,by=c("CD_HAB_8"="CD_HAB"))
+  }
+  
+  if(FCD_TYPO!=22){
+    CORRESP22 = HABREF_CORRESP_HAB_70 %>% 
+      filter(CD_TYPO_ENTRE == FCD_TYPO & CD_TYPO_SORTIE == 22) %>%
+      select(CD_HAB_ENTRE,CD_HAB_22 = CD_HAB_SORTIE)
+    data_CD_HAB=left_join(data_CD_HAB,CORRESP22,by="CD_HAB_ENTRE")
+    join22 = HABREF_70 %>% select(CD_HAB,CD_CB = LB_CODE,LB_CB = LB_HAB_FR)
+    data_CD_HAB = left_join(data_CD_HAB,join22,by=c("CD_HAB_22"="CD_HAB"))
+  }
+  
+  return(data_CD_HAB)
+}
+########################################################## 
 
 
 ############################################################   hab_match
 # Nécessite d'avoir chargé un tableau de correspondance habitats - taxons avec la fonction taxon_hab
 hab_match = function(data_flore,
                      dataflore_type = "kit_bota",
+                     flore_path = "Flore/Flore.shp",
                      groupe_type = "hab",
                      FCD_TYPO = 28, #Typologie d'habitats 28 = PVF2, 107 = EUNIS 202
                      seuil = 2 # Nombre d'espèce correspondante par groupe d'analyse
-                     ){
+){
   # utiliser les chemins du kit_bota Qfield
   if(dataflore_type == "kit_bota"){
     data_flore = st_read("Flore/Flore.shp")
@@ -83,12 +115,12 @@ hab_match = function(data_flore,
   }
   
   
-  intersection_habflore = st_intersection(data_flore, data_hab)
+  intersection_habflore = st_intersection(data_hab,data_flore)
   intersection_habflore = intersection_habflore %>% select(CD_NOM,LB_NOM = lb_nom,HABLABEL = hablabel,habid,geometry)
-
+  
   #Chargement du référentiel
   TYPO_HAB = taxon_hab(FCD_TYPO = FCD_TYPO)
-    #boucle de mise en interaction flore/habitats
+  #boucle de mise en interaction flore/habitats
   init = 0
   for(i in 1:nrow(data_hab)){
     floreturn = intersection_habflore %>% filter(habid == data_hab$habid[i])
@@ -107,11 +139,12 @@ hab_match = function(data_flore,
       }
     }
     
-      }
-
-   
+  }
+  habjoin = hab_join(habmatch$CD_HAB,FCD_TYPO = 28)
+  habmatch = left_join(habmatch,habjoin,by=c("CD_HAB"="CD_HAB_ENTRE"))
+  
   return(habmatch)
-
-
+  
+  
 }
 
